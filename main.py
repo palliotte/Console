0f5205afc047f99071d03d252729629ed189cd4f
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-10 17:45:14 +0800

    Update pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+sprite_2: game.LedSprite = None
+sprite_3: game.LedSprite = None
 my_sprite: game.LedSprite = None
 my_sprite = game.create_sprite(2, 4)
 game.set_score(0)
@@ -10,18 +12,28 @@ def on_button_pressed_b():
     my_sprite.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-def on_forever():
-    coin: game.LedSprite = None
-    coin = game.create_sprite(randint(0, 4), 0)
-    coin.set(LedSpriteProperty.BRIGHTNESS, 50)
-    basic.pause(200)
+def on_logo_pressed():
+    global sprite_3, sprite_2
+    sprite_3 = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
+    sprite_3.set(LedSpriteProperty.BRIGHTNESS, 50)
     for index in range(4):
-        coin.change(LedSpriteProperty.Y, 1)
+        sprite_3.change(LedSpriteProperty.Y, -1)
         basic.pause(400)
-        if coin.is_touching(my_sprite):
-            coin.delete()
+        if sprite_3.is_touching(sprite_2) :
+            sprite_2.delete()
+            sprite_3.delete()
             game.add_score(1)
-    if coin.is_touching_edge() :
+input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+
+def on_forever():
+    global sprite_2
+    sprite_2 = game.create_sprite(randint(0, 4), 0)
+    sprite_2.set(LedSpriteProperty.BRIGHTNESS, 50)
+    basic.pause(200)
+    for index2 in range(4):
+        sprite_2.change(LedSpriteProperty.Y, 1)
+        basic.pause(800)
+    if sprite_2.is_touching_edge():
         game.game_over()
-    coin.delete()    
+    sprite_2.delete()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+let sprite_2 : game.LedSprite = null
+let sprite_3 : game.LedSprite = null
 let my_sprite : game.LedSprite = null
 my_sprite = game.createSprite(2, 4)
 game.setScore(0)
@@ -7,23 +9,33 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     my_sprite.change(LedSpriteProperty.X, 1)
 })
-basic.forever(function on_forever() {
-    let coin : game.LedSprite = null
-    coin = game.createSprite(randint(0, 4), 0)
-    coin.set(LedSpriteProperty.Brightness, 50)
-    basic.pause(200)
+input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
+    
+    sprite_3 = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
+    sprite_3.set(LedSpriteProperty.Brightness, 50)
     for (let index = 0; index < 4; index++) {
-        coin.change(LedSpriteProperty.Y, 1)
+        sprite_3.change(LedSpriteProperty.Y, -1)
         basic.pause(400)
-        if (coin.isTouching(my_sprite)) {
-            coin.delete()
+        if (sprite_3.isTouching(sprite_2)) {
+            sprite_2.delete()
+            sprite_3.delete()
             game.addScore(1)
         }
         
     }
-    if (coin.isTouchingEdge()) {
+})
+basic.forever(function on_forever() {
+    
+    sprite_2 = game.createSprite(randint(0, 4), 0)
+    sprite_2.set(LedSpriteProperty.Brightness, 50)
+    basic.pause(200)
+    for (let index2 = 0; index2 < 4; index2++) {
+        sprite_2.change(LedSpriteProperty.Y, 1)
+        basic.pause(800)
+    }
+    if (sprite_2.isTouchingEdge()) {
         game.gameOver()
     }
     
-    coin.delete()
+    sprite_2.delete()
 })
