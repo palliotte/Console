00acd72889de5f4d5ed97afff47b83dc579c35f7
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-10 18:21:43 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,29 +1,29 @@
-sprite_2: game.LedSprite = None
-sprite_3: game.LedSprite = None
-my_sprite: game.LedSprite = None
-my_sprite = game.create_sprite(2, 4)
-game.set_score(0)
-
 def on_button_pressed_a():
     my_sprite.change(LedSpriteProperty.X, -1)
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    my_sprite.change(LedSpriteProperty.X, 1)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-def on_logo_pressed():
-    global sprite_3, sprite_2
+def on_button_pressed_ab():
+    global sprite_3
     sprite_3 = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
     sprite_3.set(LedSpriteProperty.BRIGHTNESS, 50)
     for index in range(4):
         sprite_3.change(LedSpriteProperty.Y, -1)
         basic.pause(400)
-        if sprite_3.is_touching(sprite_2) :
+        if sprite_3.is_touching(sprite_2):
             sprite_2.delete()
             sprite_3.delete()
             game.add_score(1)
-input.on_logo_event(TouchButtonEvent.PRESSED, on_logo_pressed)
+input.on_button_pressed(Button.AB, on_button_pressed_ab)
+
+def on_button_pressed_b():
+    my_sprite.change(LedSpriteProperty.X, 1)
+input.on_button_pressed(Button.B, on_button_pressed_b)
+
+sprite_2: game.LedSprite = None
+sprite_3: game.LedSprite = None
+my_sprite: game.LedSprite = None
+my_sprite = game.create_sprite(2, 4)
+game.set_score(0)
 
 def on_forever():
     global sprite_2
--- main.ts
+++ main.ts
@@ -1,7 +1,8 @@
-input.onButtonPressed(Button.A, function () {
+input.onButtonPressed(Button.A, function on_button_pressed_a() {
     my_sprite.change(LedSpriteProperty.X, -1)
 })
-input.onButtonPressed(Button.AB, function () {
+input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+    
     sprite_3 = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
     sprite_3.set(LedSpriteProperty.Brightness, 50)
     for (let index = 0; index < 4; index++) {
@@ -12,26 +13,29 @@ input.onButtonPressed(Button.AB, function () {
             sprite_3.delete()
             game.addScore(1)
         }
+        
     }
 })
-input.onButtonPressed(Button.B, function () {
+input.onButtonPressed(Button.B, function on_button_pressed_b() {
     my_sprite.change(LedSpriteProperty.X, 1)
 })
-let sprite_2: game.LedSprite = null
-let sprite_3: game.LedSprite = null
-let my_sprite: game.LedSprite = null
+let sprite_2 : game.LedSprite = null
+let sprite_3 : game.LedSprite = null
+let my_sprite : game.LedSprite = null
 my_sprite = game.createSprite(2, 4)
 game.setScore(0)
-basic.forever(function () {
+basic.forever(function on_forever() {
+    
     sprite_2 = game.createSprite(randint(0, 4), 0)
     sprite_2.set(LedSpriteProperty.Brightness, 50)
     basic.pause(200)
-    for (let index = 0; index < 4; index++) {
+    for (let index2 = 0; index2 < 4; index2++) {
         sprite_2.change(LedSpriteProperty.Y, 1)
         basic.pause(800)
     }
     if (sprite_2.isTouchingEdge()) {
         game.gameOver()
     }
+    
     sprite_2.delete()
 })
