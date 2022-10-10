a4249a973dd5639336778e71a170db3b3c48816d
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-10 18:15:34 +0800

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,4 @@
-let sprite_2 : game.LedSprite = null
-let sprite_3 : game.LedSprite = null
-let my_sprite : game.LedSprite = null
-my_sprite = game.createSprite(2, 4)
-game.setScore(0)
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    my_sprite.change(LedSpriteProperty.X, -1)
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    my_sprite.change(LedSpriteProperty.X, 1)
-})
-input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
-    
+input.onPinPressed(TouchPin.P0, function () {
     sprite_3 = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
     sprite_3.set(LedSpriteProperty.Brightness, 50)
     for (let index = 0; index < 4; index++) {
@@ -21,21 +9,29 @@ input.onLogoEvent(TouchButtonEvent.Pressed, function on_logo_pressed() {
             sprite_3.delete()
             game.addScore(1)
         }
-        
     }
 })
-basic.forever(function on_forever() {
-    
+input.onButtonPressed(Button.A, function () {
+    my_sprite.change(LedSpriteProperty.X, -1)
+})
+input.onButtonPressed(Button.B, function () {
+    my_sprite.change(LedSpriteProperty.X, 1)
+})
+let sprite_2: game.LedSprite = null
+let sprite_3: game.LedSprite = null
+let my_sprite: game.LedSprite = null
+my_sprite = game.createSprite(2, 4)
+game.setScore(0)
+basic.forever(function () {
     sprite_2 = game.createSprite(randint(0, 4), 0)
     sprite_2.set(LedSpriteProperty.Brightness, 50)
     basic.pause(200)
-    for (let index2 = 0; index2 < 4; index2++) {
+    for (let index = 0; index < 4; index++) {
         sprite_2.change(LedSpriteProperty.Y, 1)
         basic.pause(800)
     }
     if (sprite_2.isTouchingEdge()) {
         game.gameOver()
     }
-    
     sprite_2.delete()
 })
