468e1d8215319a2bfc33ebfa092cfa9e2682fb91
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-10 18:28:54 +0800

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -3,15 +3,15 @@ def on_button_pressed_a():
 input.on_button_pressed(Button.A, on_button_pressed_a)
 
 def on_button_pressed_ab():
-    global sprite_3
-    sprite_3 = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
-    sprite_3.set(LedSpriteProperty.BRIGHTNESS, 50)
+    global bullet
+    bullet = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
+    bullet.set(LedSpriteProperty.BRIGHTNESS, 50)
     for index in range(4):
-        sprite_3.change(LedSpriteProperty.Y, -1)
+        bullet.change(LedSpriteProperty.Y, -1)
         basic.pause(300)
-        if sprite_3.is_touching(sprite_2):
-            sprite_2.delete()
-            sprite_3.delete()
+        if bullet.is_touching(ennemie):
+            ennemie.delete()
+            bullet.delete()
             game.add_score(1)
 input.on_button_pressed(Button.AB, on_button_pressed_ab)
 
@@ -19,21 +19,21 @@ def on_button_pressed_b():
     my_sprite.change(LedSpriteProperty.X, 1)
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-sprite_2: game.LedSprite = None
-sprite_3: game.LedSprite = None
+ennemie: game.LedSprite = None
+bullet: game.LedSprite = None
 my_sprite: game.LedSprite = None
 my_sprite = game.create_sprite(2, 4)
 game.set_score(0)
 
 def on_forever():
-    global sprite_2
-    sprite_2 = game.create_sprite(randint(0, 4), 0)
-    sprite_2.set(LedSpriteProperty.BRIGHTNESS, 50)
+    global ennemie
+    ennemie = game.create_sprite(randint(0, 4), 0)
+    ennemie.set(LedSpriteProperty.BRIGHTNESS, 50)
     basic.pause(200)
     for index2 in range(4):
-        sprite_2.change(LedSpriteProperty.Y, 1)
+        ennemie.change(LedSpriteProperty.Y, 1)
         basic.pause(800)
-    if sprite_2.is_touching_edge():
+    if ennemie.is_touching_edge():
         game.game_over()
-    sprite_2.delete()
+    ennemie.delete()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -3,14 +3,14 @@ input.onButtonPressed(Button.A, function on_button_pressed_a() {
 })
 input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
     
-    sprite_3 = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
-    sprite_3.set(LedSpriteProperty.Brightness, 50)
+    bullet = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
+    bullet.set(LedSpriteProperty.Brightness, 50)
     for (let index = 0; index < 4; index++) {
-        sprite_3.change(LedSpriteProperty.Y, -1)
+        bullet.change(LedSpriteProperty.Y, -1)
         basic.pause(300)
-        if (sprite_3.isTouching(sprite_2)) {
-            sprite_2.delete()
-            sprite_3.delete()
+        if (bullet.isTouching(ennemie)) {
+            ennemie.delete()
+            bullet.delete()
             game.addScore(1)
         }
         
@@ -19,23 +19,23 @@ input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
 input.onButtonPressed(Button.B, function on_button_pressed_b() {
     my_sprite.change(LedSpriteProperty.X, 1)
 })
-let sprite_2 : game.LedSprite = null
-let sprite_3 : game.LedSprite = null
+let ennemie : game.LedSprite = null
+let bullet : game.LedSprite = null
 let my_sprite : game.LedSprite = null
 my_sprite = game.createSprite(2, 4)
 game.setScore(0)
 basic.forever(function on_forever() {
     
-    sprite_2 = game.createSprite(randint(0, 4), 0)
-    sprite_2.set(LedSpriteProperty.Brightness, 50)
+    ennemie = game.createSprite(randint(0, 4), 0)
+    ennemie.set(LedSpriteProperty.Brightness, 50)
     basic.pause(200)
     for (let index2 = 0; index2 < 4; index2++) {
-        sprite_2.change(LedSpriteProperty.Y, 1)
+        ennemie.change(LedSpriteProperty.Y, 1)
         basic.pause(800)
     }
-    if (sprite_2.isTouchingEdge()) {
+    if (ennemie.isTouchingEdge()) {
         game.gameOver()
     }
     
-    sprite_2.delete()
+    ennemie.delete()
 })
