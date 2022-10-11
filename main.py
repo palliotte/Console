e178bfe12c23ba26c09463dbe2b739b133692b3a
palliotte <114391946+palliotte@users.noreply.github.com>
2022-10-11 11:54:40 +0800

    Update pxt.json, main.ts, main.py

--- main.py
+++ main.py
@@ -1,39 +1,58 @@
-def on_button_pressed_a():
-    my_sprite.change(LedSpriteProperty.X, -1)
-input.on_button_pressed(Button.A, on_button_pressed_a)
+def on_pin_pressed_p0(): #mars attack
 
-def on_button_pressed_ab():
-    global bullet
-    bullet = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
-    bullet.set(LedSpriteProperty.BRIGHTNESS, 50)
-    for index in range(4):
-        basic.pause(300)
-        bullet.change(LedSpriteProperty.Y, -1)
-        if bullet.is_touching(ennemie):
-            ennemie.delete()
-            bullet.delete()
+    ennemie: game.LedSprite = None
+    bullet: game.LedSprite = None
+    my_sprite: game.LedSprite = None
+    my_sprite = game.create_sprite(2, 4)
+    game.set_score(0)
+    
+    def on_button_pressed_a():
+        my_sprite.change(LedSpriteProperty.X, -1)
+    input.on_button_pressed(Button.A, on_button_pressed_a)
+
+    def on_button_pressed_b():
+        my_sprite.change(LedSpriteProperty.X, 1)
+    input.on_button_pressed(Button.B, on_button_pressed_b)
+
+    def on_button_pressed_ab():
+        bullet = game.create_sprite(my_sprite.get(LedSpriteProperty.X), 4)
+        bullet.set(LedSpriteProperty.BRIGHTNESS, 50)
+        for index in range(4):
+            basic.pause(300)
+            bullet.change(LedSpriteProperty.Y, -1)
+            if bullet.is_touching(ennemie):
+                ennemie.delete()
+                bullet.delete()
+                game.add_score(1)
+    input.on_button_pressed(Button.AB, on_button_pressed_ab)
+    
+    def on_forever():
+        ennemie = game.create_sprite(randint(0, 4), 0)
+        ennemie.set(LedSpriteProperty.BRIGHTNESS, 50)
+        basic.pause(200)
+        for index2 in range(4):
+            ennemie.change(LedSpriteProperty.Y, 1)
+            basic.pause(800)
+        if ennemie.is_touching_edge():
+            game.game_over()
+        ennemie.delete()
+    basic.forever(on_forever)
+
+input.on_pin_pressed(TouchPin.P0, on_pin_pressed_p0)
+
+def on_pin_pressed_p01(): #snap the dot
+    my_sprite = game.create_sprite(2, 2)
+    game.set_score(0)
+    def on_button_pressed_a():
+        if my_sprite.get(LedSpriteProperty.X) == 2:
             game.add_score(1)
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
+        else:
+            game.game_over()
+    input.on_button_pressed(Button.A, on_button_pressed_a)
 
-def on_button_pressed_b():
-    my_sprite.change(LedSpriteProperty.X, 1)
-input.on_button_pressed(Button.B, on_button_pressed_b)
-
-ennemie: game.LedSprite = None
-bullet: game.LedSprite = None
-my_sprite: game.LedSprite = None
-my_sprite = game.create_sprite(2, 4)
-game.set_score(0)
-
-def on_forever():
-    global ennemie
-    ennemie = game.create_sprite(randint(0, 4), 0)
-    ennemie.set(LedSpriteProperty.BRIGHTNESS, 50)
-    basic.pause(200)
-    for index2 in range(4):
-        ennemie.change(LedSpriteProperty.Y, 1)
-        basic.pause(800)
-    if ennemie.is_touching_edge():
-        game.game_over()
-    ennemie.delete()
-basic.forever(on_forever)
+    def on_forever():
+        my_sprite.move(1)
+        my_sprite.if_on_edge_bounce()
+        basic.pause(300)
+    basic.forever(on_forever)
+input.on_pin_pressed(TouchPin.P1, on_pin_pressed_p01)
--- main.ts
+++ main.ts
@@ -1,41 +1,60 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    my_sprite.change(LedSpriteProperty.X, -1)
-})
-input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
-    
-    bullet = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
-    bullet.set(LedSpriteProperty.Brightness, 50)
-    for (let index = 0; index < 4; index++) {
-        basic.pause(300)
-        bullet.change(LedSpriteProperty.Y, -1)
-        if (bullet.isTouching(ennemie)) {
-            ennemie.delete()
-            bullet.delete()
-            game.addScore(1)
+input.onPinPressed(TouchPin.P0, function on_pin_pressed_p0() {
+    // mars attack
+    let ennemie : game.LedSprite = null
+    let bullet : game.LedSprite = null
+    let my_sprite : game.LedSprite = null
+    my_sprite = game.createSprite(2, 4)
+    game.setScore(0)
+    input.onButtonPressed(Button.A, function on_button_pressed_a() {
+        my_sprite.change(LedSpriteProperty.X, -1)
+    })
+    input.onButtonPressed(Button.B, function on_button_pressed_b() {
+        my_sprite.change(LedSpriteProperty.X, 1)
+    })
+    input.onButtonPressed(Button.AB, function on_button_pressed_ab() {
+        let bullet = game.createSprite(my_sprite.get(LedSpriteProperty.X), 4)
+        bullet.set(LedSpriteProperty.Brightness, 50)
+        for (let index = 0; index < 4; index++) {
+            basic.pause(300)
+            bullet.change(LedSpriteProperty.Y, -1)
+            if (bullet.isTouching(ennemie)) {
+                ennemie.delete()
+                bullet.delete()
+                game.addScore(1)
+            }
+            
+        }
+    })
+    basic.forever(function on_forever() {
+        let ennemie = game.createSprite(randint(0, 4), 0)
+        ennemie.set(LedSpriteProperty.Brightness, 50)
+        basic.pause(200)
+        for (let index2 = 0; index2 < 4; index2++) {
+            ennemie.change(LedSpriteProperty.Y, 1)
+            basic.pause(800)
+        }
+        if (ennemie.isTouchingEdge()) {
+            game.gameOver()
         }
         
-    }
+        ennemie.delete()
+    })
 })
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    my_sprite.change(LedSpriteProperty.X, 1)
-})
-let ennemie : game.LedSprite = null
-let bullet : game.LedSprite = null
-let my_sprite : game.LedSprite = null
-my_sprite = game.createSprite(2, 4)
-game.setScore(0)
-basic.forever(function on_forever() {
-    
-    ennemie = game.createSprite(randint(0, 4), 0)
-    ennemie.set(LedSpriteProperty.Brightness, 50)
-    basic.pause(200)
-    for (let index2 = 0; index2 < 4; index2++) {
-        ennemie.change(LedSpriteProperty.Y, 1)
-        basic.pause(800)
-    }
-    if (ennemie.isTouchingEdge()) {
-        game.gameOver()
-    }
-    
-    ennemie.delete()
+input.onPinPressed(TouchPin.P1, function on_pin_pressed_p01() {
+    // snap the dot
+    let my_sprite = game.createSprite(2, 2)
+    game.setScore(0)
+    input.onButtonPressed(Button.A, function on_button_pressed_a() {
+        if (my_sprite.get(LedSpriteProperty.X) == 2) {
+            game.addScore(1)
+        } else {
+            game.gameOver()
+        }
+        
+    })
+    basic.forever(function on_forever() {
+        my_sprite.move(1)
+        my_sprite.ifOnEdgeBounce()
+        basic.pause(300)
+    })
 })
